4bd9f5c3e32ccb11ee87d89b0a6d06cbdda127ec
soohyun <soohyun6879@naver.com>
2021-12-01 11:55:02 +0900

    백준 2470 문제풀이

---
+++ baekjoon/2470.py
@@ -0,0 +1,22 @@
+n = int(input())
+sol = list(map(int, input().split()))
+sol.sort()
+
+left, right = 0, n-1
+result = sol[left] + sol[right]
+al, ar = left, right
+
+while left < right:
+    s = sol[left] + sol[right]
+
+    if abs(s) < abs(result):
+        result = s
+        al = left
+        ar = right
+
+    if s < 0:
+        left += 1
+    else:
+        right -= 1
+
+print(sol[al], sol[ar])
