b8b2fcdf16109ead2f45cd56c1abfc5630b3b37e
soohyun <soohyun6879@naver.com>
2021-12-29 14:26:43 +0900

    백준 2565 문제풀이

---
+++ baekjoon/2565.py
@@ -0,0 +1,10 @@
+n = int(input())
+line = [list(map(int, input().split())) for _ in range(n)]
+line.sort()
+dp = [1] * n
+
+for i in range(1, n):
+    for j in range(i):
+        if line[i][1] > line[j][1]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(n - max(dp))
