fdee06d69a71053ca28c2bde4eb4f30b61f9d82a
soohyun <soohyun6879@naver.com>
2022-01-10 17:54:39 +0900

    프로그래머스 17682, 64064

---
+++ programmers/17682.py
@@ -0,0 +1,22 @@
+def solution(dartResult):
+    score = []
+    bonus = {'S': 1, 'D': 2, 'T': 3}
+    num = ''
+    
+    for d in dartResult:
+        if d.isdigit():
+            num += d
+        elif d in 'SDT':
+            score.append(int(num))
+            num = ''
+            score[-1] **= bonus[d]
+        elif d == '*':
+            if len(score) == 1:
+                score[-1] *= 2
+            else:
+                score[-1] *= 2
+                score[-2] *= 2
+        elif d == '#':
+            score[-1] = -score[-1]   
+    
+    return sum(score)
