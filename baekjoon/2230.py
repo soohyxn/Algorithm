8ff8687cf7aceddad284ccd4c4e7f73d54f8e18b
soohyun <soohyun6879@naver.com>
2022-02-08 11:28:28 +0900

    백준 2230 문제풀이

---
+++ baekjoon/2230.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+a = [int(input()) for _ in range(n)]
+a.sort()
+
+l, r = 0, 1
+ans = a[n-1] - a[0]
+
+while l < n and r < n:
+    diff = a[r] - a[l] # 수의 차이
+
+    if diff < m:
+        r += 1
+    else:
+        ans = min(ans, diff)
+        if diff == m: break
+        l += 1
+
+print(ans)
