664332b504f21df64c622a4ed0f4d4d21e308e4d
soohyun <soohyun6879@naver.com>
2022-04-14 19:19:45 +0900

    백준 15686 문제풀이

--- baekjoon/15686.py
+++ baekjoon/15686.py
@@ -1,25 +1,22 @@
 from itertools import combinations
 
-N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
+n, m = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
 house, chicken = [], [] # 집과 치킨집 위치
-answer = 1e9 # 치킨거리 최소값
+ans = 1e9 # 도시의 치킨거리 최소값
 
-# 집과 치킨집 위치 구하기
-for i in range(N):
-    for j in range(N):
-        if graph[i][j] == 1:
+for i in range(n):
+    for j in range(n):
+        if board[i][j] == 1:
             house.append([i, j])
-        elif graph[i][j] == 2:
+        elif board[i][j] == 2:
             chicken.append([i, j])
 
-coms = list(combinations(chicken, M)) # M개의 치킨집 조합
-
-for com in coms:
+for com in combinations(chicken, m):
     dist = [1e9] * len(house) # 각 집의 치킨거리
-    for cx, cy in com:
-        for i in range(len(house)):
-            dist[i] = min(dist[i], abs(cx - house[i][0]) + abs(cy - house[i][1]))
-    answer = min(answer, sum(dist))
+    for i, h in enumerate(house):
+        for c in com:
+            dist[i] = min(dist[i], abs(h[0] - c[0]) + abs(h[1] - c[1]))
+    ans = min(ans, sum(dist))
 
-print(answer)
+print(ans)
