5be2ec2a578e38ecdc6d309a78d966e8bf57023a
soohyun <soohyun6879@naver.com>
2022-04-30 23:49:32 +0900

    백준 삼성 기출문제 풀이 7

--- baekjoon/21610.py
+++ baekjoon/21610.py
@@ -4,7 +4,7 @@ move = [list(map(int, input().split())) for _ in range(m)]
 # 서, 서북, 북, 동북, 동, 동남, 남, 서남
 dx = [0, -1, -1, -1, 0, 1, 1, 1]
 dy = [-1, -1, 0, 1, 1, 1, 0, -1]
-cloud, new_cloud = [[n-1, 0], [n-1, 1], [n-2, 0], [n-2, 1]], [] # 현재 구름 위치, 다음 구름 위치
+cloud = [[n-1, 0], [n-1, 1], [n-2, 0], [n-2, 1]] # 현재 구름 위치
 
 for d, s in move:
     d -= 1
@@ -18,7 +18,7 @@ for d, s in move:
         board[nx][ny] += 1
         visited[nx][ny] = 1
         loc.append([nx, ny])
-    
+
     # 물복사 버그 마법으로 해당 위치의 물의 양이 증가
     for x, y in loc:
         for i in [1, 3, 5, 7]:
@@ -28,12 +28,11 @@ for d, s in move:
                 board[x][y] += 1
 
     # 다음 구름 위치 구하기
-    for i in range(n):
-        for j in range(n):
-            if board[i][j] >= 2 and visited[i][j] == 0:
-                new_cloud.append([i, j])
-                board[i][j] -= 2
-
-    cloud, new_cloud = new_cloud, []
+    cloud = []
+    for x in range(n):
+        for y in range(n):
+            if board[x][y] >= 2 and not visited[x][y]:
+                cloud.append([x, y])
+                board[x][y] -= 2
 
 print(sum([sum(b) for b in board])) # 물의 양의 합 
