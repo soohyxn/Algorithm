103d3bf1102a6bd578663b28f6860ba7abeefcc1
soohyun <soohyun6879@naver.com>
2022-07-06 20:48:41 +0900

    백준 7576 문제풀이

--- baekjoon/7576.py
+++ baekjoon/7576.py
@@ -1,34 +1,35 @@
 from collections import deque
 
-m, n = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+M, N = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0] # 동남서북
 queue = deque()
-answer = 0
+ans = 0
 
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1:
+# 토마토 위치 큐에 삽입
+for i in range(N):
+    for j in range(M):
+        if board[i][j] == 1:
             queue.append([i, j])
 
+# 인접한 토마토 익기
 def bfs():
     while queue:
         x, y = queue.popleft()
         for i in range(4):
-            xi = x + dx[i]
-            yi = y + dy[i]
-            if xi <= -1 or xi >= n or yi <= -1 or yi >= m:
-                continue
-            if graph[xi][yi] == 0:
-                queue.append([xi, yi])
-                graph[xi][yi] = graph[x][y] + 1
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 0: # 범위 내에 있으며 안 익은 토마토인 경우
+                queue.append([nx, ny])
+                board[nx][ny] = board[x][y] + 1
 
 bfs()
-for g in graph:
-    for v in g:
-        if v == 0:
+# 토마토가 모두 익을 최소 날짜 구하기
+for i in range(N):
+    for j in range(M):
+        if board[i][j] == 0:
             print(-1)
             exit(0)
-    answer = max(answer, max(g))
-print(answer - 1)
+        ans = max(ans, board[i][j])
+
+print(ans - 1)
