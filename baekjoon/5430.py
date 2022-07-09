8e8a241bdcc5334c67df84ec9641532d3f9ae095
soohyun <soohyun6879@naver.com>
2022-07-09 16:18:57 +0900

    백준 5430 문제풀이

--- baekjoon/5430.py
+++ baekjoon/5430.py
@@ -1,31 +1,36 @@
 from collections import deque
 
-t = int(input())
-for _ in range(t):
-    p_list = list(input())
+T = int(input())
+for _ in range(T):
+    p = list(input())
     n = int(input())
-    nums = deque(input()[1:-1].split(','))
+    x = deque(input()[1: -1].split(','))
+    cnt, flag = 0, False # 배열을 뒤집는 횟수, 에러 발생 여부
 
-    res, flag = 0, 0
+    # 배열이 빈 경우
+    if n == 0: x = deque([])
 
-    if n == 0:
-        nums = []
-
-    for p in p_list:
-        if p == 'R':
-            res += 1
-        elif  p == 'D':
-            if nums:
-                if res % 2 == 0:
-                    nums.popleft()
+    # 함수 실행
+    for i in p:
+        # 배열을 뒤집는 경우
+        if i == 'R':
+            cnt += 1
+        # 배열의 첫번째 수를 제거하는 경우
+        else:
+            # 배열이 비어있지 않다면 횟수에 따른 숫자 제거
+            if x:
+                if cnt % 2 == 0:
+                    x.popleft()
                 else:
-                    nums.pop()
+                    x.pop()
+            # 배열이 비어 있는 경우 에러 발생
             else:
                 print('error')
-                flag = 1
+                flag = True
                 break
-
-    if flag == 0:
-        if res % 2 != 0:
-            nums.reverse()
-        print('['+ ','.join(nums) +']')
+    
+    # 에러가 발생하지 않았다면 결과 출력
+    if not flag:
+        if cnt % 2 == 1:
+            x.reverse()
+        print('[' + ','.join(x) + ']')
