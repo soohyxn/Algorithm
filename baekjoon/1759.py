e0979a1995dd50ab3fd67a7e4f053dcbc6ae0bbc
soohyun <soohyun6879@naver.com>
2022-07-10 13:36:41 +0900

    백준 1759 문제풀이

--- baekjoon/1759.py
+++ baekjoon/1759.py
@@ -3,12 +3,13 @@ from itertools import combinations
 L, C = map(int, input().split())
 alpha = list(input().split())
 alpha.sort()
-combs = combinations(alpha, L)
 
-for comb in combs:
-    cnt = 0 # 모음 개수
-    for c in comb:
+# 가능한 암호 구하기
+for com in combinations(alpha, L):
+    cnt = 0 # 모음의 개수
+    for c in com:
         if c in 'aeiou':
             cnt += 1
-    if cnt >= 1 and L-cnt >= 2: # 모음 최소 1개 이상, 자음 최소 2개 이상
-        print(''.join(comb))
+    
+    if cnt >= 1 and L - cnt >= 2: # 모음이 최소 1개 이상, 자음이 최소 2개 이상인 경우
+        print(''.join(com))
