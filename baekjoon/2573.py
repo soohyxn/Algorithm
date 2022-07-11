0253395507b23bb591a4b4fb3cd6dde07b8f1cf1
soohyun <soohyun6879@naver.com>
2022-07-11 21:39:28 +0900

    백준 2573 문제풀이

--- baekjoon/2573.py
+++ baekjoon/2573.py
@@ -1,10 +1,9 @@
 from collections import deque
 
 N, M = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
-dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1]
-ans = 0 # 빙산이 분리되는 시간
-flag = False # 빙산이 분리되는지 체크
+board = [list(map(int, input().split())) for _ in range(N)]
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
+ans = 1 # 빙산이 분리되는 시간
 
 # 빙산 녹이기
 def melt():
@@ -13,20 +12,20 @@ def melt():
     # 빙산의 녹은 높이 구하기
     for x in range(N):
         for y in range(M):
-            if graph[x][y] != 0:
+            if board[x][y] > 0:
                 for i in range(4):
                     nx = x + dx[i]
                     ny = y + dy[i]
-                    if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 0:
+                    if 0 <= nx < N and 0 <= ny < M and board[nx][ny] == 0:
                         height[x][y] -= 1
 
     # 녹은 높이만큼 빙산 높이 변경
     for x in range(N):
         for y in range(M):
-            if graph[x][y] + height[x][y] < 0: # 높이는 0보다 더 줄어들 수 없다
-                graph[x][y] = 0
-            else:
-                graph[x][y] += height[x][y]
+            if board[x][y] + height[x][y] >= 0:
+                board[x][y] += height[x][y]
+            else: # 최저 높이는 0
+                board[x][y] = 0
 
 # 빙산 덩어리 구하기
 def bfs(i, j):
@@ -38,31 +37,32 @@ def bfs(i, j):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
-                if graph[nx][ny] != 0:
-                    queue.append([nx, ny])
-                    visited[nx][ny] = 1
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and board[nx][ny] > 0:
+                queue.append([nx, ny])
+                visited[nx][ny] = 1
 
 while True:
     melt() # 빙산 녹이기
 
-    cnt = 0
     visited = [[0] * M for _ in range(N)]
+    cnt = 0 # 빙산 덩어리 개수
 
     # 빙산 덩어리 구하기
     for x in range(N):
         for y in range(M):
-            if not visited[x][y] and graph[x][y] != 0:
+            if not visited[x][y] and board[x][y] > 0:
                 bfs(x, y)
                 cnt += 1
 
+    # 빙산이 다 녹을 때까지 분리되지 않는 경우
+    if cnt == 0:
+        print(0)
+        exit(0)
+
+    # 빙산의 덩어리가 2 이상인 경우 종료
+    if cnt >= 2:
+        break
+
     ans += 1
 
-    if cnt == 0: # 빙산이 다 녹을 때까지 분리되지 않는 경우
-        flag = True
-        break
-
-    if cnt >= 2: # 빙산의 덩어리가 2이상인 경우
-        break
-
-print(0) if flag else print(ans)
+print(ans)
