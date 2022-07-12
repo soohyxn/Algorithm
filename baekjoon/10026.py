48767e9ef0fd554da0368d7901d985427796c84f
soohyun <soohyun6879@naver.com>
2022-07-12 20:34:28 +0900

    백준 10026 문제풀이

--- baekjoon/10026.py
+++ baekjoon/10026.py
@@ -1,41 +1,40 @@
 from collections import deque
 
-n = int(input())
-no_rg = [list(input()) for _ in range(n)]
-yes_rg = [[''] * n for _ in range(n)]
-for i in range(n):
-    for j in range(n):
-        if no_rg[i][j] == 'R' or no_rg[i][j] == 'G':
-            yes_rg[i][j] = 'R'
-        else:
+N = int(input())
+no_rg = [list(input()) for _ in range(N)] # 적록색약이 아닌 사람
+yes_rg = [[''] * N for _ in range(N)] # 적록색약인 사람
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0] # 동남서북
+ans = [0, 0] # 구역의 수
+
+for i in range(N):
+    for j in range(N):
+        if no_rg[i][j] == 'B':
             yes_rg[i][j] = 'B'
+        else:
+            yes_rg[i][j] = 'R' # 적록색약인 사람은 빨강과 초록을 구분하지 못한다
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
-answer = [0] * 2
-
-def bfs(graph, i, j, c):
+def bfs(i, j, board, target):
     queue = deque([[i, j]])
-    graph[i][j] = 'F'
+    board[i][j] = 'X' # 방문 처리
 
     while queue:
         x, y = queue.popleft()
         for i in range(4):
-            xi = x + dx[i]
-            yi = y + dy[i]
-            if xi <= -1 or xi >= n or yi <= -1 or yi >= n:
-                continue
-            if graph[xi][yi] == c:
-                queue.append([xi, yi])
-                graph[xi][yi] = 'F'
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 아직 방문하지 않았으며 같은 색인 경우
+            if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == target:
+                queue.append([nx, ny])
+                board[nx][ny] = 'X'
 
-for i in range(n):
-    for j in range(n):
-        if no_rg[i][j] != 'F':
-            bfs(no_rg, i, j, no_rg[i][j])
-            answer[0] += 1
-        if yes_rg[i][j] != 'F':
-            bfs(yes_rg, i, j, yes_rg[i][j])
-            answer[1] += 1
+# 구역의 수 구하기
+for i in range(N):
+    for j in range(N):
+        if no_rg[i][j] != 'X':
+            bfs(i, j, no_rg, no_rg[i][j])
+            ans[0] += 1
+        if yes_rg[i][j] != 'X':
+            bfs(i, j, yes_rg, yes_rg[i][j])
+            ans[1] += 1
 
-print(*answer)
+print(*ans)
