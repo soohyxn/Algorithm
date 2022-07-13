f2b665a2375626a7bcef07578baf76cc02519d27
soohyun <soohyun6879@naver.com>
2022-07-13 20:56:53 +0900

    백준 12865 문제풀이

--- baekjoon/12865.py
+++ baekjoon/12865.py
@@ -1,13 +1,13 @@
 N, K = map(int, input().split())
-thing = [list(map(int, input().split())) for _ in range(N)]
-dp = [[0 for _ in range(K+1)] for _ in range(N+1)]
+items = [list(map(int, input().split())) for _ in range(N)]
+dp = [[0] * (K+1) for _ in range(N+1)]
 
 for i in range(1, N+1):
     for j in range(1, K+1):
-        w, v = thing[i-1]
-        if j < w: # 현재물건의 무게가 탐색하는 무게보다 작다면 이전값을 그대로 가져온다
+        W, V = items[i-1] # 탐색하는 물건
+        if j < W: # 헤딩 무게가 탐색하는 물건의 무게보다 작다면 담을 수 없으므로 이전값을 그대로 가져온다
             dp[i][j] = dp[i-1][j]
-        else: # 크다면 이전값과 현재 물건을 넣어 다른 물건들로 채운 값을 비교하여 큰 값을 저정한다
-            dp[i][j] = max(dp[i-1][j], v + dp[i-1][j-w])
+        else: # 크다면 이전값과 현재 물건을 넣고 다른 물건들로 채운 값을 비교하여 큰 값을 저장한다
+            dp[i][j] = max(dp[i-1][j], V + dp[i-1][j-W])
 
-print(dp[N][K])
+print(dp[N][K]) # 배낭에 넣을 수 있는 최대 가치합
