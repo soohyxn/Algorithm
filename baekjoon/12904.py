fe8654ed1f547729ca86ab4b393134daac9ebe7c
soohyun <soohyun6879@naver.com>
2022-07-19 22:52:41 +0900

    백준 12904 문제풀이

--- baekjoon/12904.py
+++ baekjoon/12904.py
@@ -4,7 +4,6 @@ T = list(input())
 while len(S) != len(T):
     last = T[-1]
     T.pop() # 마지막 문자 삭제
-    if last == 'B': # 마지막 문자가 B라면 뒤집기
-        T = T[::-1]
+    if last == 'B': T = T[::-1] # 마지막 문자가 B라면 뒤집기
 
 print(1) if S == T else print(0)
