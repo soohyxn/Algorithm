53cfb32f303097451178a42df33449f27e6c3e2a
soohyun <soohyun6879@naver.com>
2022-07-20 20:52:16 +0900

    백준 1916 문제풀이

--- baekjoon/1916.py
+++ baekjoon/1916.py
@@ -1,32 +1,34 @@
-from collections import defaultdict
-import sys, heapq
-input = sys.stdin.readline
+import heapq
 
-n = int(input())
-m = int(input())
-graph = defaultdict(list)
-dist = [sys.maxsize for _ in range(n+1)]
+N = int(input())
+M = int(input())
+graph = [[] for _ in range(N+1)]
+dist = [1e9] * (N+1) # 각 도시의 최단거리
 
-for _ in range(m):
-    A, B, C = map(int, input().split())
-    graph[A].append((B, C))
-S, E = map(int, input().split())
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    graph[a].append([b, c])
 
+start, end = map(int, input().split()) # 출발점, 도착점
 
+# 최단거리 구하기
 def dijkstra(start):
-    queue = []
+    heap = []
+    heapq.heappush(heap, (0, start))
     dist[start] = 0
-    heapq.heappush(queue, (0, start))
 
-    while queue:
-        cur_dist, node = heapq.heappop(queue)
-        if cur_dist > dist[node]:
+    while heap:
+        cost, node = heapq.heappop(heap) # 가장 비용이 적은 노드 가져오기
+        # 최단거리를 구한 도시인 경우
+        if dist[node] < cost:
             continue
-        for next_node, next_dist in graph[node]:
-            cost = cur_dist + next_dist
-            if cost < dist[next_node]:
-                dist[next_node] = cost
-                heapq.heappush(queue, (cost, next_node))
+        # 인접한 도시의 최단거리 구하기
+        for next in graph[node]:
+            next_cost = cost + next[1]
+            # 현재 노드를 거치는 경우가 최단거리인 경우
+            if next_cost < dist[next[0]]:
+                heapq.heappush(heap, (next_cost, next[0]))
+                dist[next[0]] = next_cost
 
-dijkstra(S)
-print(dist[E])
+dijkstra(start)
+print(dist[end])
