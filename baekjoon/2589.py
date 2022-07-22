68cd8244dc018cbdea91fc28deccdc4211b44583
soohyun <soohyun6879@naver.com>
2022-07-22 21:35:20 +0900

    백준 2589 문제풀이

--- baekjoon/2589.py
+++ baekjoon/2589.py
@@ -1,13 +1,14 @@
 from collections import deque
 
-n, m = map(int, input().split())
-map = [list(input()) for _ in range(n)]
-dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1] # 북동남서
+N, M = map(int, input().split())
+board = [list(input()) for _ in range(N)]
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0] # 동남서북
 ans = 0 # 최대 최단거리
 
+# 보물이 묻혀 있는 두 곳 간의 최단 거리 구하기
 def bfs(i, j):
     queue = deque([[i, j]])
-    visited = [[0] * m for _ in range(n)]
+    visited = [[0] * M for _ in range(N)] # 각 위치의 최단거리
     visited[i][j] = 1
 
     while queue:
@@ -15,16 +16,19 @@ def bfs(i, j):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and map[nx][ny] == 'L': # 이동 가능한 범위이며 아직 방문하지 않았고 육지인 경우
+            # 이동 가능한 범위이며 아직 방문하지 않았고 육지인 경우 다음 위치로 이동한다
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny] and board[nx][ny] == 'L':
                 queue.append([nx, ny])
                 visited[nx][ny] = visited[x][y] + 1
 
-    return max([max(v) for v in visited]) - 1
+    return max([max(v) for v in visited]) - 1 # 최단거리
 
-for i in range(n):
-    for j in range(m):
-        if map[i][j] == 'L': # 모든 육지에서 최단거리를 구한다
-            dist = bfs(i, j)
-            ans = max(ans, dist)
+# 모든 육지의 최단거리 구하기
+for i in range(N):
+    for j in range(M):
+        # 해당 위치가 육지라면 보물을 묻을 수 있으므로 최단거리를 구한다
+        if board[i][j] == 'L':
+            result = bfs(i, j)
+            ans = max(ans, result)
 
 print(ans)
