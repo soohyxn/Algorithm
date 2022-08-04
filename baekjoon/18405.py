a7a7084a7504680d964de2d7807fe4dcf63e26e3
soohyun <soohyun6879@naver.com>
2022-08-04 20:49:40 +0900

    백준 18405 문제풀이

--- baekjoon/18405.py
+++ baekjoon/18405.py
@@ -1,32 +1,31 @@
 from collections import deque
 
 N, K = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(N)]
+board = [list(map(int, input().split())) for _ in range(N)]
 S, X, Y = map(int, input().split())
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
-queue = []
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0]
 
+queue = []
 for i in range(N):
     for j in range(N):
-        if graph[i][j] != 0:
-            queue.append([graph[i][j], i, j, 0]) # 바이러스 종류, x위치, y위치, 시간
-
-queue.sort() # 오름차순 정렬
+        if board[i][j] != 0:
+            queue.append([board[i][j], i, j, 0]) # 바이러스 종류, x위치, y위치, 시간
+queue.sort() # 바이러스 종류 오름차순 정렬
 queue = deque(queue)
 
 def bfs():
     while queue:
-        virus, x, y, time = queue.popleft()
-        if time == S: # S초가 되면 멈춤
+        num, x, y, time = queue.popleft()
+        # S초가 되면 종료
+        if time == S:
             break
-        # 상, 하, 좌, 우 확인
         for i in range(4):
-            xi = x + dx[i]
-            yi = y + dy[i]
-            if 0 <= xi < N and 0 <= yi < N and graph[xi][yi] == 0: # 범위 안에 있고 바이러스가 들어가지 않은 상태라면
-                graph[xi][yi] = virus
-                queue.append([virus, xi, yi, time + 1])
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # 이동 가능한 범위이며 빈칸인 경우 바이러스가 퍼진다
+            if 0 <= nx < N and 0 <= ny < N and board[nx][ny] == 0:
+                queue.append([num, nx, ny, time+1])
+                board[nx][ny] = num
 
 bfs()
-print(graph[X-1][Y-1])
+print(board[X-1][Y-1])
