f3cea787fa1f66cf3895a400f713e0fafe4c087a
soohyun <soohyun6879@naver.com>
2022-08-12 23:06:14 +0900

    백준 17142 문제풀이

--- baekjoon/17142.py
+++ baekjoon/17142.py
@@ -1,44 +1,45 @@
 from collections import deque
 from itertools import combinations
 
-n, m = map(int, input().split())
-graph = [list(map(int, input().split())) for _ in range(n)]
+N, M = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(N)]
 dx, dy = [0, 1, 0, -1], [1, 0, -1, 0] # 동남서북
-virus, ans =  [], [] # 바이러스 위치 리스트, 바이러스를 퍼뜨리는 시간 리스트
-cnt_wall = 0 # 벽의 개수
+virus, wall = [], 0 # 바이러스 위치 리스트, 벽의 개수
+ans = [] # 바이러스를 퍼뜨리는 시간 리스트
 
-# 바이러스 위치와 벽의 개수 구하기
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 1:
-            cnt_wall += 1
-        elif graph[i][j] == 2:
+for i in range(N):
+    for j in range(N):
+        if board[i][j] == 1:
+            wall += 1
+        elif board[i][j] == 2:
             virus.append([i, j])
 
 def bfs(queue):
     time = 0 # 바이러스를 퍼뜨리는 시간
-    for i, j in queue:
-        visited[i][j] = 0 # 바이러스 위치는 0초로
+    # 바이러스 위치는 0초로
+    for i, j in queue: 
+        visited[i][j] = 0
 
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            # 이동 가능한 범위이며 벽이 아니고 아직 방문하지 않은 경우 바이러스를 퍼뜨린다
-            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] != 1 and visited[nx][ny] == -1:
+            # 이동 가능한 범위이며 벽이 아니고 아직 방문하지 않은 경우 바이러스가 퍼진다
+            if 0 <= nx < N and 0 <= ny < N and board[nx][ny] != 1 and visited[nx][ny] == -1:
                 queue.append([nx, ny])
                 visited[nx][ny] = visited[x][y] + 1
-                if graph[nx][ny] == 0: # 빈칸인 경우 시간 갱신
+                # 빈칸인 경우만 시간 갱신
+                if board[nx][ny] == 0:
                     time = max(time, visited[nx][ny])
 
-    # 방문하지 않는 곳의 개수가 벽의 개수와 동일하다면 모든 빈칸에 바이러스가 퍼진 것이므로 시간을 저장
-    if list(sum(visited, [])).count(-1) == cnt_wall:
+    # 모든 빈칸에 바이러스가 퍼졌다면 시간 저장
+    if list(sum(visited, [])).count(-1) == wall:
         ans.append(time)
 
-for com in combinations(virus, m):
+for com in combinations(virus, M):
     queue = deque(list(com))
-    visited = [[-1] * n for _ in range(n)]
+    visited = [[-1] * N for _ in range(N)]
     bfs(queue)
 
 print(min(ans) if ans else -1)
