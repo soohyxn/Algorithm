aa4c1494c7f5beb9a235525f6d45dcb71615825a
soohyun <soohyun6879@naver.com>
2022-08-15 11:59:43 +0900

    백준 17609 문제풀이

--- baekjoon/17609.py
+++ baekjoon/17609.py
@@ -4,17 +4,20 @@ for _ in range(T):
     s = input()
     l = len(s)
 
+    # 회문인 경우
     if s == s[::-1]:
         print(0)
         continue
 
     for i in range(l//2):
         if s[i] != s[l-i-1]:
-            s1 = s[:l-i-1] + s[l-i:] # 오른쪽 문자열 삭제
-            s2 = s[:i] + s[i+1:] # 왼쪽 문자열 삭제
+            s1 = s[:i] + s[i+1:] # 왼쪽 문자 삭제
+            s2 = s[:l-i-1] + s[l-i:] # 오른쪽 문자 삭제
 
+            # 유사 회문인 경우
             if s1 == s1[::-1] or s2 == s2[::-1]:
                 print(1)
+            # 일반 문자열인 경우
             else:
                 print(2)
             break
