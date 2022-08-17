aaab504932826b180867d540cba666f8745026d1
soohyun <soohyun6879@naver.com>
2022-08-17 22:27:47 +0900

    백준 2110 문제풀이

--- baekjoon/2110.py
+++ baekjoon/2110.py
@@ -1,20 +1,21 @@
 N, C = map(int, input().split())
 house = [int(input()) for _ in range(N)]
-house.sort() # 오름차순 정렬
+house.sort()
 low, high = 1, house[-1] - house[0]
 
+# 이분탐색
 while low <= high:
     mid = (low + high) // 2
     cur = house[0] # 공유기를 설치한 마지막 위치
-    count = 1 # 설치한 공유기 개수
+    cnt = 1 # 설치한 공유기 개수
 
-		# 앞 집부터 공유기 설치
+    # 공유기 설치
     for i in range(1, N):
         if house[i] >= cur + mid:
             cur = house[i]
-            count += 1
+            cnt += 1
 
-    if count >= C:
+    if cnt >= C:
         low = mid + 1
     else:
         high = mid - 1
