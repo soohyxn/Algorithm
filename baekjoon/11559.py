7fd3012914a58b6695d524a9b1a4bb039ae1cda5
soohyun <soohyun6879@naver.com>
2022-08-29 20:33:41 +0900

    백준 11559 문제풀이

--- baekjoon/11559.py
+++ baekjoon/11559.py
@@ -1,59 +1,65 @@
 from collections import deque
 import sys
-
 input = sys.stdin.readline
-graph = [list(input()) for _ in range(12)]
-dx, dy = [-1, 0, 1, 0], [0, 1, 0, -1] # 북동남서
+
+board = [list(input()) for _ in range(12)]
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0] # 동남서북
 ans = 0 # 연쇄 횟수
 
 # 뿌요 없애기
-def bfs(i, j, state):
+def bfs(i, j, color):
     queue = deque([[i, j]])
-    puyo = [[i, j]] # 같은 색의 뿌요 리스트
     visited[i][j] = 1
+    puyo = [[i, j]] # 같은색의 뿌요 리스트
 
     while queue:
         x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            # 이동 가능한 범위에 있고 아직 방문하지 않은 뿌요이며 현재 뿌요와 색깔이 같은 경우
-            if 0 <= nx < 12 and 0 <= ny < 6 and not visited[nx][ny] and graph[nx][ny] == state:
+            # 이동 가능한 범위에 있으며 아직 방문하지 않았고 같은색의 뿌요인 경우
+            if 0 <= nx < 12 and 0 <= ny < 6 and not visited[nx][ny] and board[nx][ny] == color:
                 queue.append([nx, ny])
-                puyo.append([nx, ny])
                 visited[nx][ny] = 1
+                puyo.append([nx, ny])
 
-    # 같은 색의 뿌요가 4개 이상이면 없앤다
+    # 같은색의 뿌요가 4개 이상이면 없어진다
     if len(puyo) >= 4:
         for x, y in puyo:
-            graph[x][y] = '.'
-    
-    return len(puyo)
+            board[x][y] = '.'
+        return True
+    else:
+        return False
 
-# 뿌요가 아래로 떨어지기
+# 뿌요 아래로 떨어지기
 def down():
     for j in range(6):
-        for i in range(10, -1, -1):
-            for k in range(11, i, -1):
-                if graph[i][j] != '.' and graph[k][j] == '.':
-                    graph[k][j] = graph[i][j]
-                    graph[i][j] = '.'
-                    break
+        queue = deque() # 뿌요 리스트
+        # 뿌요를 리스트에 저장한다
+        for i in range(11, -1, -1):
+            if board[i][j] != '.':
+                queue.append(board[i][j])
+        # 뿌요를 아래로 옮기고 나머지는 빈칸으로 채운다
+        for i in range(11, -1, -1):
+            if queue:
+                board[i][j] = queue.popleft()
+            else:
+                board[i][j] = '.'
 
 while True:
     visited = [[0] * 6 for _ in range(12)]
-    flag = True # 터트릴 뿌요가 없는지 체크
+    flag = False # 뿌요가 있는지 체크
 
-    # 뿌요 없애기
     for i in range(12):
         for j in range(6):
-            if not visited[i][j] and graph[i][j] != '.':
-                if bfs(i, j, graph[i][j]) >= 4: # 터트릴 뿌요가 있다면 flag = False
-                    flag = False
+            if not visited[i][j] and board[i][j] != '.':
+                if bfs(i, j, board[i][j]):
+                    flag = True 
 
-    if flag: break
+    # 터트릴 뿌요가 없는 경우 종료
+    if not flag: break
 
     ans += 1
-    down() # 뿌요 아래로 떨어지기
+    down()
 
 print(ans)
