6bbb5fed024822e3ca34fedb083d8c564361fe61
soohyun <soohyun6879@naver.com>
2023-01-29 11:21:15 +0900

    백준 1806 문제풀이

--- baekjoon/1806.py
+++ baekjoon/1806.py
@@ -1,19 +1,21 @@
-n, s = map(int, input().split())
+N, S = map(int, input().split())
 nums = list(map(int, input().split()))
-l, r = 0, 1 # 투 포인터
+sum = [0] * (N+1) # 누적합
+left, right = 0, 1 # 투포인터
 ans = 1e9 # 최소 길이
-sum_n = [0] * (n+1) # 부분합 리스트
 
-for i in range(1, n+1):
-    sum_n[i] = sum_n[i-1] + nums[i-1]
+# 누적합 구하기
+for i in range(1, N+1):
+    sum[i] = sum[i-1] + nums[i-1]
 
-while l < n+1 and r < n+1:
-    result = sum_n[r] - sum_n[l] # l부터 r까지의 합
+# 투포인터
+while left <= N and right <= N:
+    result = sum[right] - sum[left] # 부분합
 
-    if result >= s: # 합이 s이상이면 최소길이를 갱신 왼쪽 포인터 이동
-        ans = min(ans, r-l)
-        l += 1
-    else: # 합이 s미만이면 오른쪽 포인터 이동
-        r += 1
+    if result >= S: # 부분합이 S이상이면 길이 갱신, 왼쪽 포인터 이동
+        ans = min(ans, right - left)
+        left += 1
+    else: # 아니라면 오른쪽 포인터 이동
+        right += 1
 
-print(0) if ans == 1e9 else print(ans)
+print(0 if ans == 1e9 else ans)
