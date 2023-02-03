75cf40af963dd2e4e4dde9196235c523df5c2fa0
soohyun <soohyun6879@naver.com>
2023-02-03 21:38:00 +0900

    백준 2493 문제풀이

--- baekjoon/2493.py
+++ baekjoon/2493.py
@@ -1,15 +1,16 @@
-n = int(input())
+N = int(input())
 towers = list(map(int, input().split()))
 stack = []
-answer = [0] * n
+ans = [0] * N
 
-for i in range(n):
+for i in range(N):
+    # 수신탑을 구한다
     while stack:
-        if stack[-1][0] > towers[i]:
-            answer[i] = stack[-1][1] + 1
+        if stack[-1][0] > towers[i]: # 레이저를 수신할 수 있다면 저장 후 종료
+            ans[i] = stack[-1][1]
             break
-        else:
+        else: # 그렇지 않다면 스택에서 제거
             stack.pop()
-    stack.append((towers[i], i))
+    stack.append([towers[i], i+1])
 
-print(*answer)
+print(*ans)
