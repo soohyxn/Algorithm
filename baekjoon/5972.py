814de39dade13a5849bdbd436fb5041b9c2aebd2
soohyun <soohyun6879@naver.com>
2023-02-08 22:23:37 +0900

    백준 5972 문제풀이

--- baekjoon/5972.py
+++ baekjoon/5972.py
@@ -2,33 +2,33 @@ import heapq
 
 N, M = map(int, input().split())
 graph = [[] for _ in range(N+1)]
-dist = [1e9] * (N+1) # 헛간별 최단 경로
+dist = [1e9] * (N+1) # 각 헛간의 최소 여물 비용
 
 for _ in range(M):
     a, b, c = map(int, input().split())
     graph[a].append([b, c])
     graph[b].append([a, c])
 
-def dijkstra(start):
+def dijkstra():
     queue = []
-    # 시작 노드는 큐에 삽입하고 최단 경로는 0으로 설정
-    heapq.heappush(queue, (0, start))
-    dist[start] = 0
+    heapq.heappush(queue, (0, 1))
+    dist[1] = 0
 
+    # 최소 비용을 구한다
     while queue:
-        d, node = heapq.heappop(queue) # 거리, 현재 노드
-        
-        # 현재 노드가 이미 처리된 적이 있다면 무시
-        if dist[node] < d:
+        cost, node = heapq.heappop(queue) # 비용, 헛간 번호
+
+        # 최소 비용보다 현재 비용이 크다면 넘어간다
+        if dist[node] < cost:
             continue
 
-        # 현재 노드와 인접한 노드들 확인
-        for i in graph[node]:
-            cost = d + i[1]
-            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 빠른 경우
-            if cost < dist[i[0]]:
-                dist[i[0]] = cost
-                heapq.heappush(queue, (cost, i[0]))
+        # 연결된 모든 헛간 탐색
+        for x, y in graph[node]:
+            new_cost = cost + y
+            # 새로운 비용이 최소 비용보다 작다면 갱신한다
+            if new_cost < dist[x]:
+                heapq.heappush(queue, (new_cost, x))
+                dist[x] = new_cost
 
-dijkstra(1)
+dijkstra()
 print(dist[N])
