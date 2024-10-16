6747d30ff8fcbcc4d9b55815865af7ca12af1d96
soohyun <soohyun6879@naver.com>
2024-10-16 11:25:38 +0900

    백준 2178 문제풀이

--- baekjoon/2178.py
+++ baekjoon/2178.py
@@ -1,27 +1,28 @@
 from collections import deque
 
 n, m = map(int, input().split())
-graph = [list(map(int, input())) for _ in range(n)]
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
+board = [list(map(int, input())) for _ in range(n)]
+dx, dy = [0, 1, 0, -1], [1, 0, -1, 0] # 동남서북
 
-def bfs(i, j):
-    queue = deque([[i, j]])
+def bfs(a, b):
+    queue = deque([[a, b]])
 
     while queue:
         x, y = queue.popleft()
+
+        # 4방향 탐색
         for i in range(4):
             xi = x + dx[i]
             yi = y + dy[i]
-            if xi <= -1 or xi >= n or yi <= -1 or yi >= m:
+
+            # 미로의 범위를 벗어나면 넘어간다
+            if xi < 0 or xi >= n or yi < 0 or yi >= m:
                 continue
-            if graph[xi][yi] == 1:
+            
+            # 이동할 수 있으면 이동 칸수 계산
+            if board[xi][yi] == 1:
                 queue.append([xi, yi])
-                graph[xi][yi] = graph[x][y] + 1
+                board[xi][yi] = board[x][y] + 1
 
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1:
-            bfs(i, j)
-
-print(graph[n-1][m-1])
+bfs(0, 0)
+print(board[n-1][m-1])
