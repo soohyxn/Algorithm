cddb569d8caed2a0e988b2b405fb4ed1af2d03a3
soohyun <soohyun6879@naver.com>
2024-10-16 10:28:46 +0900

    백준 1260 문제풀이

--- baekjoon/1260.py
+++ baekjoon/1260.py
@@ -1,36 +1,42 @@
+from collections import deque
+
 n, m, v = map(int, input().split())
 graph = [[] for _ in range(n+1)]
-visited = [0] * (n+1)
+visited_dfs = [0] * (n+1)
+visited_bfs = [0] * (n+1)
 
 for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
 
+# 정점번호가 작은순으로 방문하도록 정렬
 for g in graph:
     g.sort()
 
-def dfs(start):
-    visited[start] = 1
-    print(start, end=' ')
+def dfs(node):
+    visited_dfs[node] = 1
+    print(node, end=' ')
 
-    for i in graph[start]:
-        if not visited[i]:
-            dfs(i)
+    # 다음 정점 탐색
+    for next in graph[node]:
+        if not visited_dfs[next]:
+            dfs(next)
 
 def bfs(start):
-    queue = [start]
-    visited[start] = 1
+    queue = deque([start])
+    visited_bfs[start] = 1
 
     while queue:
-        node = queue.pop(0)
+        node = queue.popleft()
         print(node, end=' ')
-        for i in graph[node]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = 1
+
+        # 다음 정점 탐색
+        for next in graph[node]:
+            if not visited_bfs[next]:
+                queue.append(next)
+                visited_bfs[next] = 1
 
 dfs(v)
 print()
-visited = [0] * (n+1)
 bfs(v)
