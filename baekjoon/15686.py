0485f965e328abc20c31d62e9188b5d8e628ab47
soohyun <soohyun6879@naver.com>
2024-10-17 10:38:28 +0900

    백준 15686 문제풀이

--- baekjoon/15686.py
+++ baekjoon/15686.py
@@ -2,9 +2,10 @@ from itertools import combinations
 
 n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
-house, chicken = [], [] # 집과 치킨집 위치
-ans = 1e9 # 도시의 치킨거리 최소값
+house, chicken = [], [] # 집 위치, 치킨집 위치
+answer = 1e9 # 최소 도시의 치킨 거리
 
+# 집과 치킨집 위치 구하기
 for i in range(n):
     for j in range(n):
         if board[i][j] == 1:
@@ -12,11 +13,15 @@ for i in range(n):
         elif board[i][j] == 2:
             chicken.append([i, j])
 
+# 가장 수익을 많이 낼 모든 치킨집 경우 탐색
 for com in combinations(chicken, m):
-    dist = [1e9] * len(house) # 각 집의 치킨거리
-    for i, h in enumerate(house):
-        for c in com:
-            dist[i] = min(dist[i], abs(h[0] - c[0]) + abs(h[1] - c[1]))
-    ans = min(ans, sum(dist))
+    city_dist = [] # 도시의 치킨 거리 (= 모든 집의 치킨 거리의 합)
+    # 집의 치킨 거리 구하기
+    for hx, hy in house:
+        house_dist = 1e9 # 최소 집의 치킨 거리
+        for cx, cy in com:
+            house_dist = min(house_dist, abs(cx - hx) + abs(cy - hy))
+        city_dist.append(house_dist)
+    answer = min(answer, sum(city_dist))    
 
-print(ans)
+print(answer)
