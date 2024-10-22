4d4bb44b3d54fb585c96ade9e34f916853d921c0
soohyun <soohyun6879@naver.com>
2024-10-22 09:52:46 +0900

    백준 11052 문제풀이

--- baekjoon/11052.py
+++ baekjoon/11052.py
@@ -1,9 +1,9 @@
 n = int(input())
-cards = [0] + list(map(int, input().split()))
-dp = [c for c in cards]
+cards = list(map(int, input().split()))
+dp = cards[:] # 카드비용의 최대값 리스트
 
-for i in range(2, n+1):
-    for j in range(1, i//2+1):
-        dp[i] = max(dp[i], dp[j] + dp[i-j])
+for i in range(n):
+    for j in range(i):
+        dp[i] = max(dp[i], dp[j] + dp[i-j-1])
 
-print(dp[n])
+print(dp[n-1])
